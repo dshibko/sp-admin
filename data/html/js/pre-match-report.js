c41520dfef2a373c2361df07b3617996493ce823
panina <svetlana.panina@hiqo-solutions.com>
2013-06-14 11:32:33 +0300

    Markup. Message page

--- data/html/js/pre-match-report.js
+++ data/html/js/pre-match-report.js
@@ -27,143 +27,154 @@ $(document).ready(function () {
     });
 });
 
-function initCharts(doResize){
-
-    if (window.topScoresData !== undefined){
-        var top5ScoresChart = $.plot($('#chart_top5'),
-            window.topScoresData, {
-                series: {
-                    pie: {
-                        innerRadius: 0.4,
-                        stroke: {color: '#f2e1e7', width: 0},
-                        show: true,
-                        label: {
-                            show: false
-                        }
+var plots = [];
+if (window.topScoresData !== undefined){
+    plots.push($.plot('#chart_top5',
+        window.topScoresData, {
+            series: {
+                pie: {
+                    innerRadius: 0.4,
+                    stroke: {color: '#f2e1e7', width: 0},
+                    show: true,
+                    label: {
+                        show: false
                     }
-                }
-            });
-
-        if (doResize === true){
-            top5ScoresChart.resize();
-            top5ScoresChart.setupGrid();
-            top5ScoresChart.draw();
-        }
-
-    }
-
-
-    if ($('#chart-head-to-head-results').size() > 0){
-        var headToHeadChart = $.plot($('#chart-head-to-head-results'),
-            [
-                {color: '#363636', data: 530},
-                {color: '#144a9b', data: 193},
-                {color: '#7f7f80', data: 20}
-            ],
-            {
-                series: {
-                    pie: {
-                        innerRadius: 0.4,
-                        stroke: {color: '#e6edf8', width: 0},
-                        show: true,
-                        radius: 1,
-                        label: {
-                            show: true,
-                            radius: 2/3,
-                            formatter: function(label, series) {
-                                return series.data[0][1];
-                            }
-                        }
-                    }
-                },
-                legend: {
-                    show: false
                 }
             }
-        );
+        }));
 
-        if (doResize === true){
-            headToHeadChart.resize();
-            headToHeadChart.setupGrid();
-            headToHeadChart.draw();
-        }
-    }
-
-    if ($('#chart-away-form').size() > 0){
-        var awayFormChart = $.plot($('#chart-away-form'),
-            [
-                {color: '#363636', data: 53},
-                {color: '#144a9b', data: 41},
-                {color: '#7f7f80', data: 6}
-            ],
-            {
-                series: {
-                    pie: {
-                        innerRadius: 0.4,
-                        stroke: {color: '#e6edf8', width: 0},
-                        show: true,
-                        radius: 1,
-                        label: {
-                            show: true,
-                            radius: 2/3,
-                            formatter: function(label, series) {
-                                return series.data[0][1] + '%';
-                            }
-                        }
-                    }
-                },
-                legend: {
-                    show: false
-                }
-            }
-        );
-
-        if (doResize === true){
-            awayFormChart.resize();
-            awayFormChart.setupGrid();
-            awayFormChart.draw();
-        }
-    }
-
-    if ($('#chart-home-form').size() > 0){
-        var homeFormChart = $.plot($('#chart-home-form'),
-            [
-                {color: '#363636', data: 53},
-                {color: '#144a9b', data: 41},
-                {color: '#7f7f80', data: 6}
-            ],
-            {
-                series: {
-                    pie: {
-                        innerRadius: 0.4,
-                        stroke: {color: '#e6edf8', width: 0},
-                        show: true,
-                        radius: 1,
-                        label: {
-                            show: true,
-                            radius: 2/3,
-                            formatter: function(label, series) {
-                                return series.data[0][1]+ '%';
-                            }
-                        }
-                    }
-                },
-                legend: {
-                    show: false
-                }
-            }
-        );
-
-        if (doResize === true){
-            homeFormChart.resize();
-            homeFormChart.setupGrid();
-            homeFormChart.draw();
-        }
-    }
 }
 
-initCharts(false);
+
+
+if ($('#chart-head-to-head-results').size() > 0){
+    plots.push($.plot('#chart-head-to-head-results',
+        [
+            {color: '#363636', data: 530, label:''},
+            {color: '#144a9b', data: 193, label:''},
+            {color: '#7f7f80', data: 20,label:''}
+        ],
+        {
+            series: {
+                pie: {
+                    innerRadius: 0.4,
+                    stroke: {color: '#e6edf8', width: 0},
+                    show: true,
+                    radius: 1,
+                    label: {
+                        show: true,
+                        radius: 2/3,
+                        formatter: function(label, series) {
+                            return series.data[0][1] + label;
+                        }
+                    }
+                }
+            },
+            legend: {
+                show: false
+            }
+        }
+    ));
+}
+
+
+
+if ($('#chart-away-form').size() > 0){
+    plots.push($.plot('#chart-away-form',
+        [
+            {color: '#363636', data: 53, label : '%'},
+            {color: '#144a9b', data: 41, label : '%'},
+            {color: '#7f7f80', data: 6, label : '%'}
+        ],
+        {
+            series: {
+                pie: {
+                    innerRadius: 0.4,
+                    stroke: {color: '#e6edf8', width: 0},
+                    show: true,
+                    radius: 1,
+                    label: {
+                        show: true,
+                        radius: 2/3,
+                        formatter: function(label, series) {
+                            return series.data[0][1] + label;
+                        }
+                    }
+                }
+            },
+            legend: {
+                show: false
+            }
+        }
+    ));
+}
+
+
+
+if ($('#chart-home-form').size() > 0){
+    plots.push($.plot('#chart-home-form',
+        [
+            {color: '#363636', data: 53, label : '%'},
+            {color: '#144a9b', data: 41, label : '%'},
+            {color: '#7f7f80', data: 6, label : '%'}
+        ],
+        {
+            series: {
+                pie: {
+                    innerRadius: 0.4,
+                    stroke: {color: '#e6edf8', width: 0},
+                    show: true,
+                    radius: 1,
+                    label: {
+                        show: true,
+                        radius: 2/3,
+                        formatter: function(label, series) {
+                            return series.data[0][1]+ label;
+                        }
+                    }
+                }
+            },
+            legend: {
+                show: false
+            }
+        }
+    ));
+}
+
+// plots.push($.plot('#community-predicted-results', 
+//     [
+//       {color: '#90b1d4', data: 95},
+//       {color: '#144a9b', data: 5}
+//     ], 
+//     {
+//         series: {
+//             pie: {
+//                 innerRadius: 0.8,
+//                 stroke: {color: '#e6edf8', width: 0},
+//                 show: true,
+//                 radius: 1,
+//                 label: {
+//                     show: true,
+//                     radius: 0,
+//                     formatter: function(label, series) { 
+//                         return series.data[0][1] + '%';
+//                     }
+//                 }
+//             }
+//         },
+//         legend: {
+//             show: false
+//         }
+//     }
+// ));
+
+
+
 $(window).on('resize', function() {
-    initCharts(true);
+    for (var i = plots.length - 1; i >= 0; i--) {
+        plots[i].resize();
+        plots[i].setupGrid();
+        plots[i].draw();
+    };
 });
 
