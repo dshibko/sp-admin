616f6790e2108f9b0f5c72eb85d4919b77945347
Oleg Kovelkov <oleg.kovelkov@hiqo-solutions.com>
2013-07-08 20:46:10 +0300

    Score Predictor v2.0 Chelsea FC version (Minor Fix)

--- module/Opta/src/Opta/Manager/OptaManager.php
+++ module/Opta/src/Opta/Manager/OptaManager.php
@@ -149,20 +149,21 @@ class OptaManager extends BasicManager {
                                     $player->addCompetition($competition);
 
                                 $player->setTeam($team);
+                                $increment = $playerXml->Stat->count() == 10 ? -1 : 0;
                                 if (!$player->getIsBlocked()) {
                                     $player->setDisplayName($playerXml->Name->__toString());
-                                    $player->setShirtNumber($this->getNodeValue($playerXml->Stat, 6));
+                                    $player->setShirtNumber($this->getNodeValue($playerXml->Stat, 6 + $increment));
                                 }
                                 $player->setPosition($playerXml->Position->__toString());
                                 $player->setName($playerXml->Stat->{0}->__toString());
                                 $player->setSurname($playerXml->Stat->{1}->__toString());
-                                $player->setBirthDate($this->getNodeValue($playerXml->Stat, 3, 'Y-m-d'));
-                                $player->setWeight($this->getNodeValue($playerXml->Stat, 4));
-                                $player->setHeight($this->getNodeValue($playerXml->Stat, 5));
-                                $player->setRealPosition($this->getNodeValue($playerXml->Stat, 7));
-                                $player->setRealPositionSide($this->getNodeValue($playerXml->Stat, 8));
-                                $player->setJoinDate($this->getNodeValue($playerXml->Stat, 9, 'Y-m-d'));
-                                $player->setCountry($this->getNodeValue($playerXml->Stat, 10));
+                                $player->setBirthDate($this->getNodeValue($playerXml->Stat, 3 + $increment, 'Y-m-d'));
+                                $player->setWeight($this->getNodeValue($playerXml->Stat, 4 + $increment));
+                                $player->setHeight($this->getNodeValue($playerXml->Stat, 5 + $increment));
+                                $player->setRealPosition($this->getNodeValue($playerXml->Stat, 7 + $increment));
+                                $player->setRealPositionSide($this->getNodeValue($playerXml->Stat, 8 + $increment));
+                                $player->setJoinDate($this->getNodeValue($playerXml->Stat, 9 + $increment, 'Y-m-d'));
+                                $player->setCountry($this->getNodeValue($playerXml->Stat, 10 + $increment));
                             } catch (\Exception $e) {
                                 ExceptionManager::getInstance($this->getServiceLocator())->handleOptaException($e, Logger::ERR, $console);
                             }
