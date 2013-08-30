0be99f1e5093315d16e15879aea8df497312db98
Oleg Kovelkov <oleg.kovelkov@hiqo-solutions.com>
2013-08-31 01:02:48 +0300

    Anticache added

--- module/Custom/src/Custom/Controller/CustomHotFixesController.php
+++ module/Custom/src/Custom/Controller/CustomHotFixesController.php
@@ -2,10 +2,17 @@
 
 namespace Custom\Controller;
 
+use Application\Controller\ClearAppCacheController;
+use Application\Manager\ApplicationManager;
 use Application\Manager\ExportManager;
 use Application\Manager\FTPManager;
 use Application\Manager\LogManager;
 use Application\Manager\UserManager;
+use Application\Model\DAOs\LeagueUserDAO;
+use Application\Model\DAOs\LeagueUserPlaceDAO;
+use Application\Model\DAOs\MatchGoalDAO;
+use Application\Model\DAOs\MessageDAO;
+use Application\Model\DAOs\PredictionDAO;
 use Custom\Manager\CustomExportManager;
 use Custom\Manager\CustomHotFixManager;
 use Custom\Model\Helper\CustomMessagesConstants;
@@ -17,6 +24,24 @@ use \Zend\Mvc\Controller\AbstractActionController;
 use \Zend\Console\Exception\RuntimeException;
 
 class CustomHotFixesController extends AbstractActionController {
+
+    public function clearCacheAction() {
+        $entitiesToBeCleared = array();
+        $cacheClearArr[] = MatchGoalDAO::getInstance($this->getServiceLocator())->getRepositoryName();
+        $cacheClearArr[] = PredictionDAO::getInstance($this->getServiceLocator())->getRepositoryName();
+        $cacheClearArr[] = LeagueUserDAO::getInstance($this->getServiceLocator())->getRepositoryName();
+        $cacheClearArr[] = LeagueUserPlaceDAO::getInstance($this->getServiceLocator())->getRepositoryName();
+        $cacheClearArr[] = MessageDAO::getInstance($this->getServiceLocator())->getRepositoryName();
+
+        $entitiesToBeClearedQueryString = implode(",", $entitiesToBeCleared);
+        $clearAppCacheUrl = ApplicationManager::getInstance($this->getServiceLocator())->getClearAppCacheUrl();
+        $clearAppCacheUrl = $clearAppCacheUrl . urlencode($entitiesToBeClearedQueryString);
+        $clearAppCacheResult = file_get_contents($clearAppCacheUrl);
+        if ($clearAppCacheResult == ClearAppCacheController::OK_MESSAGE)
+            var_dump(MessagesConstants::APP_CACHE_CLEARED);
+        else
+            var_dump(MessagesConstants::APP_CACHE_NOT_CLEARED);
+    }
 
     public function privateLeagueAction() {
 
